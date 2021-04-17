ae4dc8ad7397a726e01631ef44b44642cec0ff91
Daglas Ruiz <daglasruizdev@gmail.com>
2021-04-17 16:37:20 -0400

    initial commit

---
+++ src/PersonalWebsite.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PersonalWebsite = () => {
+	return <div>hola mundo</div>;
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { PersonalWebsite } from "./PersonalWebsite";
+
+ReactDOM.render(<PersonalWebsite />, document.getElementById("root"));
